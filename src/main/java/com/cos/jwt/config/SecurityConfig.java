c0ad111074c0572eb01fc3f97e5f8b69c69d09bf
jinnyRyu <hajinryu51@gmail.com>
2021-07-25 17:40:20 +0900

    [config]jwt를 사용하기위한 filter등록

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,20 +1,29 @@
 package com.cos.jwt.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
+import lombok.RequiredArgsConstructor;
 import lombok.Builder.ObtainVia;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
+    private final CorsFilter corsFilter;
     @Override
     protected void configure(HttpSecurity http)throws Exception{
         http.csrf().disable();
         //세션을 사용하지 않겠다는말
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
+        .addFilter(corsFilter)//corssorigin인증이 필요없는경우에만사용, 시큐리티 필터에 등록하면 인증이 필요한 경우에도 사용가능
         .formLogin().disable()//기본적인 http로그인 안쓰겟다
         .httpBasic().disable()
         .authorizeRequests()
--- src/main/java/com/cos/jwt/controller/RestApiController.java
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,8 +1,9 @@
 package com.cos.jwt.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// @CrossOrigin 인증이 필요한건 거부
 @RestController
 public class RestApiController {
 
