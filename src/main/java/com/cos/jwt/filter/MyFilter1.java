d477271e294c530a9c65ba1b767b10cee596be72
jinnyRyu <hajinryu51@gmail.com>
2021-07-25 22:29:15 +0900

    filter test..

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -7,15 +7,24 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
 
 public class MyFilter1 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("filter1");
-        chain.doFilter(request, response);
-        
+                
+                HttpServletRequest req = (HttpServletRequest) request;
+                HttpServletResponse res = (HttpServletResponse) response;
+                
+                String headerAuth = req.getHeader("Authorization");
+                System.out.println(headerAuth);
+                System.out.println("filter1");
+                chain.doFilter(req, res);
     }
 
     
