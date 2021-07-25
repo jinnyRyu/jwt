a5a4559cb37a28a4d2f8f6f21430e8de752814dd
jinnyRyu <hajinryu51@gmail.com>
2021-07-25 18:32:59 +0900

    [test]filterconfig test

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     private final CorsFilter corsFilter;
     @Override
     protected void configure(HttpSecurity http)throws Exception{
+        //http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
         http.csrf().disable();
         //세션을 사용하지 않겠다는말
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
